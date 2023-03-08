4cce453639a34771f054e3aa52eb31aaf5421d31
VictorCobo2 <90394098+VictorCobo2@users.noreply.github.com>
2023-03-08 08:21:05 -0500

    get con904

--- src/controllers/LOGIN_COBOL.ts
+++ src/controllers/LOGIN_COBOL.ts
@@ -31,3 +31,31 @@ export const login_cobol = (req: Request, res: Response) => {
       });
   } catch (error) {}
 };
+
+export const GET_CON904 = async (req:Request, res:Response)=>{
+  try {
+    let { data, ip } = req.body;
+    let formData = new FormData();
+      for (let [key, value] of Object.entries(data)) formData.append(key, value);
+      const config = {
+        method: "POST",
+        url: `http://${ip}/MAIN-ELECT/APP/CONTAB/CON904.DLL`,
+        data: formData,
+      };
+      axios(config)
+      .then(function (respuesta: any) {
+        typeof respuesta.data == "object" ? res.json(respuesta.data) : res.json(JSON.parse(eliminarEspacios(respuesta.data)));
+      })
+      .catch(function (error) {
+        console.error(error);
+        res.json({ msg: error });
+      });
+  } catch (error) {
+    console.error(error);
+    res.json({ msg: error });
+  }
+}
+
+function eliminarEspacios(cadena: string) {
+  return cadena.replace(/\s+/g, " ").trim();
+}
--- src/routes/LOGIN_COBOL.ts
+++ src/routes/LOGIN_COBOL.ts
@@ -1,7 +1,9 @@
 import express from "express"
-import { login_cobol } from "../controllers/LOGIN_COBOL";
+import { GET_CON904, login_cobol } from "../controllers/LOGIN_COBOL";
 
 export const route_login_cobol = express.Router();
 
 route_login_cobol.get("/login", login_cobol)
+route_login_cobol.post("/GET_CON904", GET_CON904);
+
 
